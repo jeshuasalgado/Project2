bf7d0374f03360232f83cf1a966d3e7ee49c8777
colton bloomquist <coltonbloomquist@yahoo.com>
2021-11-03 13:23:58 -0600

    message

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,51 +1,47 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { Ingredients, Recipe, Steps } = require('../models');
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Ingredients, Recipe, Steps } = require("../models");
 
-router.get('/login', (req, res) => {
-    if(req.session.loggedIn) {
-        res.redirect('/');
-        return;
-    }
-    res.render('login');
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("login");
 });
 
-router.get('/logout', (req, res) => {
-    if(req.session.loggedIn) {
-        req.session.destroy(() => {
-            res.status(204).end();
-        })
-    } else {
-        res.status(404).end();
-    }
-})
+router.get("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 
-router.get('/', (req, res) => {
-    Recipe.findAll({
-        attributes: [
-            'id',
-            'title',
-            'recipe_url'
-        ], 
-        include: [
-            {
-                model: Ingredients,
-                attributes: ['ingredients_text', 'recipe_id']
-            },
-            {
-                model: Steps,
-                attributes: ['recipe_id']
-            }
-        ]
+router.get("/", (req, res) => {
+  Recipe.findAll({
+    attributes: ["id", "name", "picture", "ingredients_id", "steps_id"],
+    include: [
+      {
+        model: Ingredients,
+        attributes: ["name", "amount"],
+      },
+      {
+        model: Steps,
+        attributes: ["whatever is needed"],
+      },
+    ],
+  })
+    .then((dbData) => {
+      const recipes = dbData.map((recipe) => recipe.get({ plain: true }));
+      res.render("login");
     })
-    .then(dbData => {
-        const recipes = dbData.map(recipe => recipe.get({ plain: true }));
-        res.render('addrecipe');
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
