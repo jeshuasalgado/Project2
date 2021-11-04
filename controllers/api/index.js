2d006aa59951c4a9f9b5f9fa36b7f64ffc5e11a1
Riley Smith <gordonrileysmith@gmail.com>
2021-11-03 19:16:51 -0600

    adjusted routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 
-const ingredientRoutes = require('./ingredient-routes');
 const recipeRoutes = require('./recipe-routes');
 const userRoutes = require('./user-routes');
 
-router.use('/ingredients', ingredientRoutes);
 router.use('/recipes', recipeRoutes);
 router.use('/users', userRoutes);
 
